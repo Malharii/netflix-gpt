7d8ff0ecacdc3cd01b0f54c7f456c0b8837f794e
Malharii <152295830+malhariii@users.noreply.github.com>
2024-04-24 17:03:33 +0530

    maincontainer looks good

--- src/components/VideoTitel.js
+++ src/components/VideoTitel.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { FaPlay } from "react-icons/fa";
+import { IoInformationCircleOutline } from "react-icons/io5";
 
 function VideoTitel({ title, overview }) {
   return (
-    <div className=" pt-36 px-12">
-      <h1 className="text-5xl font-bold">{title}</h1>
+    <div className=" w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
+      <h1 className="text-4xl font-bold">{title}</h1>
       <p className="text-lg w-1/4 pt-2">{overview}</p>
-      <div>
-        <button className="bg-white text-black p-4 px-6 text-xl opacity-50 rounded-lg">
-          ▶️Play
+      <div className=" flex py-4 ">
+        <button className="bg-white text-black p-3 px-4 text-xl hover:opacity-80 rounded-lg flex justify-between">
+          <FaPlay className="pr-2 text-3xl " />
+          Play
         </button>
-        <button className="mx-2 bg-white text-black p-4 px-6 text-xl opacity-50 rounded-lg">
-          More Info
+        <button className="mx-2 bg-white text-black p-3 px-4 text-xl hover:opacity-60 rounded-lg flex">
+          <IoInformationCircleOutline className="mx-2 text-3xl" /> More Info
         </button>
       </div>
     </div>
